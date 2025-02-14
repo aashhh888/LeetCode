85f0b11a14449b40cf17a538883518ce60b8fef3
Aash Patel <aash.patel434@gmail.com>
2025-02-14 13:33:13 +0000

    Added Two Sum

---
+++ Easy/1. Two Sum/Final Attempt.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(0, len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:                    
+                    return [i,j]
+
+
+sol = Solution()
+#Test cases go here
+print(f"{sol.twoSum([2,7,11,15], 9) == [0,1] if True else False}")
+print(f"{sol.twoSum([3,2,4], 6) == [1,2] if True else False}")
+print(f"{sol.twoSum([3,3], 6) == [0,1] if True else False}")
