756988f68f59fbd5d447e228e6de191201ccb182
Aash Patel <aash.patel434@gmail.com>
2025-02-22 22:49:52 +0000

    Clean up in Go 26

--- Go/Easy/26. Remove Duplicates From Sorted Array/Converted.go
+++ Go/Easy/26. Remove Duplicates From Sorted Array/Converted.go
@@ -25,25 +25,3 @@ func main() {
 	fmt.Println(reflect.DeepEqual(removeDuplicates(case2), 5))
 	fmt.Println(reflect.DeepEqual(case2, []int{0, 1, 2, 3, 4, 2, 2, 3, 3, 4}))
 }
-
-// from typing import List
-
-// class Solution:
-//     def removeDuplicates(self, nums: List[int]) -> int:
-//         curr = 101
-//         for i in range(len(nums) - 1, -1, - 1):
-//             if curr == nums[i]:
-//                 nums.pop(i)
-//             else:
-//                 curr = nums[i]
-//         return len(nums)
-
-// sol = Solution()
-// #Test cases go here
-// case1 = [1,1,2]
-// print(f"{sol.removeDuplicates(case1) == 2}")
-// print(f"{case1 == [1,2]}")
-
-// case2 = [0,0,1,1,1,2,2,3,3,4]
-// print(f"{sol.removeDuplicates(case2) == 5}")
-// print(f"{case2 == [0,1,2,3,4]}")
